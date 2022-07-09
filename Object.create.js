a5dbdc19531a110fc2b823423c068d195f4bbed9
IHuan123 <whuan0322@163.com>
2022-07-09 18:13:50 +0800

    up

--- Object.create.js
+++ Object.create.js
@@ -1,6 +1,19 @@
 //Object.create实现
-function createObj(proto){
-    function F(){}
-    F.prototype = proto;
-    return new F();
+function ObjectCreate(proto, propertiesObject){
+    if (![ 'object', 'function' ].includes(typeof proto)) {
+        throw new TypeError(`Object prototype may only be an Object or null: ${proto}`)
+    }
+    let Ctor = new Function()
+    Ctor.prototype = proto
+    const res = new Ctor()
+    // 支持第二个参数
+    if (propertiesObject) {
+        // Object.defineProperty的批量处理方法
+        Object.defineProperties(res, propertiesObject)
+    }
+      // 支持空原型
+    if (proto === null) {
+        res.__proto__ = null
+    }
+    return res
 }
